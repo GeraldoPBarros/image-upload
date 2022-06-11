90487618b39fcf1b2daa9caae43dc18b05b661b5
GeraldoBarros <geraldo.barros@neurobots.com.br>
2022-06-11 19:02:43 -0300

    remocao de consoles.

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -51,9 +51,6 @@ export default async function handler(
   if (req.method === 'GET') {
     const { after } = req.query;
 
-    console.log('req.query: ', req.query);
-    console.log('AFTER SERVER: ', after);
-
     const queryOptions = {
       size: 6,
       ...(after && { after: query.Ref(query.Collection('images'), after) }),
